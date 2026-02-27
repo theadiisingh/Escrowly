fcf8984abb5b2f2d44accc7a3fb5c2d519c36a4d
theadiisingh <adityaveer.singh.2212@gmail.com>
2026-02-27 19:32:11 +0530

    Initial Spring Boot backend setup

---
+++ escrowly-backend/src/main/java/com/escrowly/escrowly_backend/EscrowlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.escrowly.escrowly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscrowlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscrowlyBackendApplication.class, args);
+	}
+
+}
---
+++ escrowly-backend/src/test/java/com/escrowly/escrowly_backend/EscrowlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escrowly.escrowly_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscrowlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
